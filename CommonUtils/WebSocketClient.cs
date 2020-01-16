f463046644f0e124be42a68ce5afd3644426aa87
蒋龙 <372792797@qq.com>
2020-01-16 14:00:23 +0800

    opt WebSocketClient

--- CommonUtils/WebSocketClient.cs
+++ CommonUtils/WebSocketClient.cs
@@ -1,12 +1,24 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using WebSocketSharp;
 
 namespace CommonUtils
 {
     public class WebSocketClient : WebSocket
     {
+        public event Action<Exception> ErrorEvent;
+        public event Action OpenEvent;
+        public event Action<string> MessageEvent;
+        public event Action CloseEvent;
+
         public WebSocketClient(string url)
-        : base(url) { }
+        : base(url)
+        {
+            OnError += delegate (object sender, ErrorEventArgs e) { ErrorEvent?.Invoke(e.Exception); };
+            OnOpen += delegate (object sender, EventArgs e) { OpenEvent?.Invoke(); };
+            OnMessage += delegate (object sender, MessageEventArgs e) { MessageEvent?.Invoke(e.Data); };
+            OnClose += delegate (object sender, CloseEventArgs e) { CloseEvent?.Invoke(); };
+        }
 
         public WebSocketClient(IPEndPoint iep, string route)
         : base("ws://" + iep + (route[0] == '/' ? route : ("/" + route))) { }
